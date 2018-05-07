995f2be4760026003a9666f8fcc55eb4f3c4c184
Allen <280894830@qq.com>
2018-05-07 16:49:37 +0800

    从控制器端改变变量传输到客户端

--- MvcMusicStore/Controllers/HomeController.cs
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MvcMusicStore.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "I Like cake!";
 
             return View();
         }
