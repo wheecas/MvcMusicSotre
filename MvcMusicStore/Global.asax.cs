b24e53f5d1e17901c56d5be015969a0248c5813a
Allen <280894830@qq.com>
2018-05-16 14:44:47 +0800

    联调数据库

--- MvcMusicStore/Global.asax.cs
+++ MvcMusicStore/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MvcMusicStore.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,9 @@ namespace MvcMusicStore
     {
         protected void Application_Start()
         {
+            //Database.SetInitializer(new DropCreateDatabaseAlways<MusicStoreDB>());
+            Database.SetInitializer(new MusicStoreDbInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
